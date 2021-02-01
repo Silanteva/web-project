e5eb27de0633d05bd857a031871faf6b22bdd199
Silanteva <71524540+Silanteva@users.noreply.github.com>
2021-02-01 12:17:14 +0300

    Add files via upload

---
+++ mySite/www/index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><META http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>
+
+
+
+<div>
+
+ <style>
+ body {
+ background: #556B2F url(https://img2.goodfon.ru/original/2048x1365/9/9a/germaniia-zamok-les-derevia-osen.jpg);
+ }
+ </style>
+
+<table border="3" align="center" rules="rows" style="width:60%">
+
+<tr>
+
+<td>
+
+
+
+
+<table border="1" background="http://images/168.png" bgcolor="#FFDAB9" cellpadding="10" style="width:100%;border-radius:5px">
+
+<tr>
+
+<th>
+
+<h1>Dusseldorf - the capital of North Rhine-Westphalia</h1>
+<h3>Description and main characteristics of the German city </h3>
+
+</th>
+</tr>
+</table>
+
+
+
+
+
+<table border="1" bgcolor="#FFDAB9" cellpadding="10" style="width:100%;border-radius:5px">
+
+<tr>
+
+<td rowspan="2" style="width:40%">
+
+<h2>More web-sites</h2>
+<div><a href="https://tonkosti.ru/%D0%94%D1%8E%D1%81%D1%81%D0%B5%D0%BB%D1%8C%D0%B4%D0%BE%D1%80%D1%84" target="_blank">
+<div>Dusseldorf</div>
+<div>Germany</div>
+<div>Traditions</div>
+<div>Education</div>
+<div>Advertising/<div>
+<div>Going abroad</div>
+</div></div></a></div>
+
+<h2>To learn more</h2>
+<div><a href="https://kidpassage.com/publications/13-interesnyih-faktov-o-dyusseldorfe" target="_blank">
+<div>Interesting facts about this town</div>
+<div>The natural originity</div>
+<div>Culture and historical buildings</div>
+<div>Songs and legends</div>
+</div></div></a></div>
+
+<h2>Test about Dusseldorf</h2>
+<div><a href="http://myforum/test.php" target="_blank">
+<div>Check your knowledge</div>
+<div>Questions on this theme</div>
+<div>How can you remember</div>
+<div>How much do you know about Dusseldorf</div>
+</div></div></a></div>
+
+</td>
+
+
+
+
+<td bgcolor="#FFDAB9" align = "center">
+<h3>The originity of the city</h3>
+
+<img src="https://baslamaodul.co/uploads/59783-Diy-elmas-boyama-%C3%A7apraz-diki%C5%9F-kristal-i%C4%9Fne-elmas-nak%C4%B1%C5%9F-elmas-dekoratif-orman-sonbahar-kalesi-peyzaj-1.jpeg" alt="https://baslamaodul.co/uploads/59783-Diy-elmas-boyama-%C3%A7apraz-diki%C5%9F-kristal-i%C4%9Fne-elmas-nak%C4%B1%C5%9F-elmas-dekoratif-orman-sonbahar-kalesi-peyzaj-1.jpeg" class="shrinkToFit" width="632" height="356">
+</td>
+</tr>
+
+<tr>
+
+<td bgcolor="#FFDAB9" align = "center" >
+
+<h2>Dusseldorf - the capital of North Rhine-Westphalia </h2>
+
+<p style="text-indent:20px">
+Dusseldorf is a large city in West Germany located 
+in the Rhine-Ruhr region, which is the official capital 
+of the federal states of North Rhine-Westphalia and the 
+seat of the government district of Dusseldorf. It is 
+located in the lower reaches of the Rhine, on its right 
+bank, at the confluence with the Dussel River. Some of 
+the districts of Dusseldorf, such as Lerik, Oberkassel, 
+Heerdt and Niederkassel are located on the left bank of 
+the Rhine. In total, the city includes 10 districts, subdivided 
+into 49 districts and numbering about 600,000 local residents. 
+</p>
+
+<p style="text-indent:20px">
+Dusseldorf is one of the 5 largest political, economic, cultural 
+and transport centers in Germany along with Hamburg, Berlin, Munich 
+and Frankfurt am Main. It is the leading center for advertising, 
+telecommunications technology and patent science in Europe. It 
+hosts the world's largest printing exhibition "Drupa" and the 
+equally popular model exhibition "Collections Premieren Dusseldorf", 
+thanks to which Dusseldorf is called the German "city of fashion". </p>
+
+
+</td>
+</tr>
+</table>
+
+
+
+
+<table border="2" bgcolor="#FFDAB9" height="100" cellpadding="10" style="width:100%;border-radius:5px">
+
+<tr>
+
+<th>
+<h3>The brilliant and extraordinary Dusseldorf is 
+among the 10 best cities in the world, it is visited
+ by millions of tourists, therefore the city has many 
+hotels, apartments, hotels and apart-hotels, guest 
+houses and hostels for every taste and budget, the 
+highest prices for accommodation are set in the central areas.</h3>
+
+</th>
+</tr>
+</table>
+
+</td>
+</tr>
+</table>
+</div>
+
+</body></html>
